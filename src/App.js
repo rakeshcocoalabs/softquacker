67562112d9045b3e12c642b727afb404aced4450
rakeshcocoalabs <docsofrakesh@gmail.com>
2020-11-06 20:19:47 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Typography } from '@material-ui/core';
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant="h3" component="h2">
+        Some thing amazing is waiting for you
+      </Typography>
+    </div>
+  );
+}
+
+export default App;
